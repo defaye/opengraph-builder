d52f8832235fa449394a784586150441aac879e8
Jonathan Feist <jonathan.feist@unep-wcmc.org>
2020-06-26 17:09:52 +0100

    Rename content_for placeholder

--- lib/opengraph_builder.rb
+++ lib/opengraph_builder.rb
@@ -30,10 +30,10 @@ module OpenGraphBuilder
     #
     def og_content(prefix = nil, **options)
       if prefix || options.empty?
-        content_for :opengraph_builder_og_content
+        content_for :opengraph_builder
       else
         options.each do |k, v|
-          content_for :opengraph_builder_og_content,
+          content_for :opengraph_builder,
                       format('<meta property="%<prefix>s:%<k>s" content="%<v>s">',
                              [prefix || 'og', k, v])
         end
